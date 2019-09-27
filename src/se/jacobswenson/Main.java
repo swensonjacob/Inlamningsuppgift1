0b6737ddc9792399358989545899b8863c6cc1f0
”Jacob <jacob.swenson@outlook.com>
2019-09-27 08:07:14 +0200

    Kanonutskrift

--- src/se/jacobswenson/Main.java
+++ src/se/jacobswenson/Main.java
@@ -3,5 +3,7 @@ package se.jacobswenson;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("klart!");    }
+        System.out.println("klart!");
+        System.out.println("Yees");
+    }
 }
